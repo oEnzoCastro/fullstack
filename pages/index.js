a7b314505459de42c8b336925d9200f7ca242b32
oEnzoCastro <enzocastru@gmail.com>
2025-01-03 16:01:55 -0300

    First

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { use, useState } from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <Contador/>
+        </div>
+    )
+}
+
+export default Home
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
